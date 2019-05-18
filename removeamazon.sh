f39b615648015f1d1c25c42d78f2c88bca075bee
remembercds <42159506+remembercds@users.noreply.github.com>
2019-05-18 17:30:45 -0400

    Add files via upload

---
+++ removeamazon.sh
@@ -0,0 +1,3 @@
+sudo rm /usr/share/applications/ubuntu-amazon-default.desktop
+sudo rm /usr/share/unity-webapps/userscripts/unity-webapps-amazon/Amazon.user.js
+sudo rm /usr/share/unity-webapps/userscripts/unity-webapps-amazon/manifest.json
---
+++ startup.sh
@@ -0,0 +1,16 @@
+
+
+#turn off auto-suspend
+sudo systemctl mask sleep.target suspend.target hibernate.target hybrid-sleep.target
+
+#configure wifi
+
+#download vpn
+
+#switch esc and caps
+setxkbmap -option caps:swapescape
+
+#change wallpaper
+wget https://i.imgur.com/sBM9y7a.png
+
+gsettings set org.gnome.desktop.background picture-uri "/root/sBM9y7a.png"
